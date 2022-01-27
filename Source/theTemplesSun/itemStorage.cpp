b1512f6c51abe78e70741a6547dcbe758e0c116d
Syed F. Ali <62857780+alis0712@users.noreply.github.com>
2022-01-27 17:35:58 -0500

    mods

---
+++ Source/theTemplesSun/itemStorage.cpp
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "itemStorage.h"
+
+// Sets default values
+AitemStorage::AitemStorage()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+// Called when the game starts or when spawned
+void AitemStorage::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
---
+++ Source/theTemplesSun/itemStorage.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "itemStorage.generated.h"
+
+UCLASS()
+class THETEMPLESSUN_API AitemStorage : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AitemStorage();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+
+
+};
